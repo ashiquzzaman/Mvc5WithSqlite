3ca5e21bab2d9829b7e4f700e86177cade5cd868
ashiquzzaman <md.ashiquzzaman@datasoft-bd.com>
2018-02-22 15:16:53 +0600

    orginized folder

---
+++ Mvc5WithSqlite/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mvc5WithSqlite.Models
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [StringLength(128)]
+        public string Name { get; set; }
+        public int DepartmentId { get; set; }
+        [ForeignKey("DepartmentId")]
+        public Department Department { get; set; }
+    }
+    public class Department
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [StringLength(128)]
+        public string Name { get; set; }
+
+        public IList<Employee> Employees { get; set; }
+    }
+}
---
+++ Mvc5WithSqlite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc5WithSqlite.Startup))]
+namespace Mvc5WithSqlite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
